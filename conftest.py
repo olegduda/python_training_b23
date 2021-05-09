a960d0ad458d8690d38145a960d5faca6a7b5c8d
o.duda <o.duda@bi.zone>
2021-05-09 18:14:40 +0300

    feat [] Increase the "intelligence" of the login and logo functions
    - Проверка перед логином, на необходимость логиниться
    - Проверка перед разлогином, на необходимость разлогиниться

--- conftest.py
+++ conftest.py
@@ -10,11 +10,11 @@ def app(request):
 
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+        fixture.session.ensure_login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+            fixture.session.ensure_login(username="admin", password="secret")
 
     return fixture
 
@@ -22,7 +22,7 @@ def app(request):
 @pytest.fixture(scope='session', autouse=True)
 def stop_session(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
